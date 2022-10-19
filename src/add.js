090f3c4819cff84dd04c706e6e2dd26cb487f886
RadekSerejch <xserej00@stud.fit.vutbr.cz>
2022-10-19 16:03:10 +0200

    add form to add new listitem

--- src/add.js
+++ src/add.js
@@ -1,9 +1,15 @@
-const addnew = () => {
-    const handleSubmit = () => {
-        console.log("button clicked");
-        const name = "galadriel";
-        const description = "rings of power wonder woman";
-        const author = "pepa";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Addnew = () => {
+    const [name,setName] = useState("");
+    const [description,setDescription] = useState("");
+    const [author,setAuthor] = useState("");
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("here");
         const data = {name, description, author};
         fetch('http://localhost:8000/items/', {
             method: 'POST',
@@ -11,14 +17,30 @@ const addnew = () => {
             body: JSON.stringify(data)
         }).then(() => {
             console.log("data sent");
+            history.push('/');
         })
     }
 
     return ( 
-        <button onClick={handleSubmit}>
-        click to add data
-        </button>
+        <div className="content">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type = "text" required value={name} onChange = {(e) => {setName(e.target.value)}}></input>
+                </label>
+                <label>
+                    description:
+                    <input type = "text" required value={description} onChange = {(e) => {setDescription(e.target.value)}}></input>
+                </label>
+                <label>
+                    Author:
+                    <input type = "text" required value={author} onChange = {(e) => {setAuthor(e.target.value)}}></input>
+                </label>
+                <button>submit</button>
+            </form>
+        </div>
+        
      );
 }
  
-export default addnew;
+export default Addnew;
